e92bb423584dfcedde03d90aacc5bdb76a2f3013
Victor Neplyuev <vic4517369@gmail.com>
2025-04-30 06:55:41 +0300

    delete button is working

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,23 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
+const cardItem = cardTemplate.querySelector('.card');
 
 // @todo: Функция создания карточки
 function cardCreate (card) {
-  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
-  cardItem.querySelector('.card__image').src = card.link;
-  cardItem.querySelector('.card__image').alt = `Фото места под названием ${card.name}`;
-  //cardItem.querySelector('.card__delete-button').addEventListener('click', deleteCard())
-  cardItem.querySelector('.card__title').textContent = card.name;
-  placesList.append(cardItem);
+  const newCard = cardItem.cloneNode(true);
+  newCard.querySelector('.card__image').src = card.link;
+  newCard.querySelector('.card__image').alt = `Фото места под названием ${card.name}`;
+  newCard.querySelector('.card__title').textContent = card.name;
+  newCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+  placesList.append(newCard);
 }
 
 // @todo: Функция удаления карточки
-/*function deleteCard () {
-  const listItem = cardItem.querySelector('.card__delete-button').closest('.todo__item');
-  listItem.remove();
-}*/
+function deleteCard () {
+  const element = placesList.querySelector('.card__delete-button').closest('.places__item');
+  element.remove();
+}
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((card) => cardCreate(card));
