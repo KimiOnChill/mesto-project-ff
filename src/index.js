57edc249b6817c82a20ee53e8917d10a10fcb3be
Victor Neplyuev <vic4517369@gmail.com>
2025-05-21 07:21:22 +0300

    fix closing and openning

--- src/index.js
+++ src/index.js
@@ -24,28 +24,41 @@ const popupAddNewCard = document.querySelector('.popup_type_new-card');
 const popupToShowFullPic = document.querySelector('.popup_type_image');
 const popupImage = document.querySelector('.popup__image');
 
-const everyButtonToClosePopup = document.querySelectorAll('.popup__close');
-
 // Вывод карточки на страницу
 initialCards.forEach((card) => cardsContainer.append(createCard(card, deleteCard)));
 
 // Обработчики событий
+
+// Открытие модального окна редактирования профиля по кнопке
 buttonOpenEditProfile.addEventListener('click', () => openModal(popupEditProfile) );
 
+// Открытие модального окна добавления карточки по кнопке
 buttonAddNewCard.addEventListener('click', () => openModal(popupAddNewCard) );
 
+// Открытие модального окна изображения карточки на фулл
+// по клику на изображение карточки
 cardsContainer.addEventListener('click', function (evt) {
-  popupImage.src = evt.target.src;
-  popupImage.alt = evt.target.alt;
-  openModal(popupToShowFullPic);
+  if (evt.target.classList.contains('card__image')) {
+    popupImage.src = evt.target.src;
+    popupImage.alt = evt.target.alt;
+    openModal(popupToShowFullPic);
+  }
+ 
 });
 
+//Закрытие любого открытого модального окна
 popupContainer.forEach( container => {
+  //закрытие кликом по крестику или оверлею
   container.addEventListener('click', function (evt) {
-    if (!evt.target.classList.contains('popup__content')) {
+    if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close')) {
       closeModal(container);
     }
   });
+  //закрытие нажатием на esc
+  document.addEventListener('keydown', function closeByEsc(evt) {
+    if (evt.key === 'Escape') {
+      closeModal(container);
+      document.removeEventListener('keydown', closeByEsc);
+    }
+  });
 });
-
-
